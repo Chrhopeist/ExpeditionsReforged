72a48e310bd69b60e9cec9c93f5160b822d9c0d2
Chrhopeist <chrhopeist@gmail.com>
2025-12-13 13:40:46 -0500

    Create ExpeditionUI.cs

---
+++ ExpeditionUI.cs
@@ -0,0 +1,24 @@
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.UI.Elements;
+using Terraria.UI;
+
+namespace ExpeditionsReforged.UI
+{
+    public class ExpeditionUI : UIState
+    {
+        private UIPanel _rootPanel;
+
+        public override void OnInitialize()
+        {
+            _rootPanel = new UIPanel();
+            _rootPanel.Left.Set(0f, 0f);
+            _rootPanel.Top.Set(0f, 0f);
+            _rootPanel.Width.Set(0f, 1f);
+            _rootPanel.Height.Set(0f, 1f);
+            _rootPanel.SetPadding(16f);
+            _rootPanel.BackgroundColor = new Color(30, 35, 52, 200);
+
+            Append(_rootPanel);
+        }
+    }
+}
