4b3eae34e06a3ce9a82da708bbaef2fd283e4dda
Chrhopeist <chrhopeist@gmail.com>
2025-12-13 13:45:38 -0500

    Wire expedition and tracker UI layers

--- Systems/ExpeditionsSystem.cs
+++ Systems/ExpeditionsSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.UI;
+using ExpeditionsReforged.UI;
 
 namespace ExpeditionsReforged.Systems
 {
@@ -11,6 +12,9 @@ namespace ExpeditionsReforged.Systems
         private UserInterface _expeditionInterface;
         private UserInterface _trackerInterface;
 
+        private ExpeditionUI _expeditionUI;
+        private TrackerUI _trackerUI;
+
         public override void Load()
         {
             if (Main.dedServ)
@@ -18,12 +22,23 @@ namespace ExpeditionsReforged.Systems
 
             _expeditionInterface = new UserInterface();
             _trackerInterface = new UserInterface();
+
+            _expeditionUI = new ExpeditionUI();
+            _expeditionUI.Activate();
+            _expeditionInterface.SetState(_expeditionUI);
+
+            _trackerUI = new TrackerUI();
+            _trackerUI.Activate();
+            _trackerInterface.SetState(_trackerUI);
         }
 
         public override void Unload()
         {
             _expeditionInterface = null;
             _trackerInterface = null;
+
+            _expeditionUI = null;
+            _trackerUI = null;
         }
 
         public override void UpdateUI(GameTime gameTime)
@@ -34,7 +49,36 @@ namespace ExpeditionsReforged.Systems
 
         public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
         {
-            // UI layers will be inserted here once UIState implementations exist.
+            int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+
+            if (mouseTextIndex != -1)
+            {
+                layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer(
+                    "ExpeditionsReforged: Tracker UI",
+                    delegate
+                    {
+                        if (_trackerInterface?.CurrentState != null)
+                        {
+                            _trackerInterface.Draw(Main.spriteBatch, new GameTime());
+                        }
+
+                        return true;
+                    },
+                    InterfaceScaleType.UI));
+
+                layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer(
+                    "ExpeditionsReforged: Expedition UI",
+                    delegate
+                    {
+                        if (_expeditionInterface?.CurrentState != null)
+                        {
+                            _expeditionInterface.Draw(Main.spriteBatch, new GameTime());
+                        }
+
+                        return true;
+                    },
+                    InterfaceScaleType.UI));
+            }
         }
     }
 }
